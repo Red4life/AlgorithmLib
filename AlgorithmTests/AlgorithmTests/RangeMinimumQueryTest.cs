d5cdcc9c409083e9489576f7bfa885694ae8d6e4
Red_4life <jdokicfan@gmail.com>
2014-06-14 19:41:55 +0100

    Initial commit

---
+++ AlgorithmTests/AlgorithmTests/RangeMinimumQueryTest.cs
@@ -0,0 +1,15 @@
+﻿using Algorithm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmTests
+{
+    [TestClass]
+    public class RangeMinimumQueryTest
+    {
+        [TestMethod]
+        public void FindMinimumTest()
+        {
+            var rmq = new RangeMinimumQuery();
+        }
+    }
+}
